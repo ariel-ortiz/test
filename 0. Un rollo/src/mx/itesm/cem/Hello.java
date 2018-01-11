a4f49a584a22991975960c3ce5ec4f4155e97b72
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2018-01-11 09:21:14 -0600

    Testing 1...

---
+++ 0. Un rollo/src/mx/itesm/cem/Hello.java
@@ -0,0 +1,12 @@
+package mx.itesm.cem;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+	}
+	
+	
+
+}
---
+++ 0. Un rollo/src/mx/itesm/cem/Other.java
@@ -0,0 +1,10 @@
+package mx.itesm.cem;
+
+public class Other {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, you");
+
+    }
+
+}
